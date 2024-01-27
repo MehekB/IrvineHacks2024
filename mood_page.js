bc98a9f0918c6d891527650f8762b0759b6960cc
MehekB <mehekbhatnagar3@gmail.com>
2024-01-26 19:46:58 -0800

    added mood text

--- mood_page.js
+++ mood_page.js
@@ -1,8 +1,16 @@
 let mood = ""
+
 function onHappyClick() {
-    alert ('Happy clicked!')
     mood = 'happy'
+    document.getElementById("textField").textContent = "Your mood is: " + mood + ".";
+    console.log(mood)
 }
+
+function onSadClick() {
+    mood = 'sad'
+    document.getElementById("textField").textContent = "Your mood is: " + mood + ".";
+}
+
 
 const button = document.querySelector('button');
 button.addEventListener('click', onHappyClick);
