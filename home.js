1a25378979d80061fa909ec4133b3cef4ca5d1a2
saraht-hub <61632695+saraht-hub@users.noreply.github.com>
2024-01-27 00:52:20 -0800

    log in to spotify front end

---
+++ home.js
@@ -0,0 +1,6 @@
+const CLIENT_ID = '92715c01f1f145969e18198e7c7d7ef5';
+const SPOTIFY_AUTHORIZE_ENDPOINT = 'https://accounts.spotify.com/authorize'
+const REDIRECT_URL_AFTER_LOGIN = 'http://localhost:5500/mood.html'
+const SCOPES = ("user-top-read");
+const SCOPES_URL_PARAM = SCOPES.join('%20');
+
