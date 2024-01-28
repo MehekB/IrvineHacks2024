3296706fbb9fb337fd2354eb99ec5f9d306684ee
saraht-hub <61632695+saraht-hub@users.noreply.github.com>
2024-01-28 04:33:10 -0800

    added api call to create a new playlist cover

--- playlist_cover.js
+++ playlist_cover.js
@@ -1,5 +1,24 @@
-let new_cover = localStorage.getItem('cover')
+let newCover = localStorage.getItem('cover')
 let at = localStorage.getItem('access_token')
+let dataURL = localStorage.getItem('imageDataURL')
+const apiUrl = `https://api.spotify.com/v1/playlists/${newCover}/images`
 
-//need to store playlist id in localStorage
+
+console.log(newCover.photoID)
 //use api for customize playlist cover
+async function createNewCover(){
+    const reseponse = await fetch(apiUrl, {
+        method: 'PUT',
+        headers: {
+        'Authorization': `Bearer `+ at,
+        "Content-type": "image/jpeg"
+        },
+        body: new URLSearchParams({
+            data: dataURL
+        })
+    })
+    const res = (await response.json())
+    console.log(newCover.photoID)
+}
+
+
