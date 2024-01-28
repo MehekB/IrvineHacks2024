e8e3b3538c9e3da3b31a0285b2b92b6874d712cd
saraht-hub <61632695+saraht-hub@users.noreply.github.com>
2024-01-27 22:32:10 -0800

    changing more parts of the drawing board

--- drawing_board.js
+++ drawing_board.js
@@ -1,22 +1,13 @@
 function captureBoard(){
-    console.log('?')
-    // Get the canvas and context
-    const canvas = document.getElementById('drawing-board-canvas');
-    const context = canvas.getContext('2d');
+    const canvas = document.getElementById('drawing-board-canvas-wrapper');
+    
+    // Convert canvas to dataURL and log to console
+    console.log(canvas)
+    const dataURL = canvas.toDataURL();
+    console.log(dataURL);
+    // Logs data:image/png;base64,wL2dvYWwgbW9yZ...
 
-    // Get the image
-    const sourceImage = document.getElementById('sourceImage');
-
-    // Set the dimensions you want to capture
-    const captureWidth = 100;
-    const captureHeight = 100;
-
-    // Draw a portion of the image onto the canvas
-    context.drawImage(sourceImage, 50, 50, captureWidth, captureHeight, 0, 0, captureWidth, captureHeight);
-
-    // Convert the canvas content to data URL
-    const imageDataURL = canvas.toDataURL('image/png');
-
-    // Do something with the imageDataURL, such as displaying it or sending it to a server
-    console.log(imageDataURL);
+    // Convert to Base64 string
+    const base64 = getBase64StringFromDataURL(dataURL);
+    console.log(base64);
 }
