5e8047e0a99a8457fd87b7526cdc074f65b316c4
saraht-hub <61632695+saraht-hub@users.noreply.github.com>
2024-01-28 10:06:40 -0800

    added connection to api changing spotify cover

--- playlist_cover.js
+++ playlist_cover.js
@@ -1,27 +1,20 @@
 let playlistID = localStorage.getItem('selectedPlaylist')
 let at = localStorage.getItem('access_token')
-let dataURL = localStorage.getItem('dataURL')
+let dataURL = localStorage.getItem('dataURL').substring(23,localStorage.getItem('dataURL').length)
+console.log('DATA URL--------',dataURL)
 const apiUrl = `https://api.spotify.com/v1/playlists/${playlistID}/images`
 
-console.log("PLAYLIST ID", playlistID)
-console.log("access token:",at)
-console.log("data url",dataURL)
-console.log("api url",apiUrl)
-
-//use api for customize playlist cover
 async function createNewCover(){
+    // try{
+        console.log(at)
     const response = await fetch(apiUrl, {
         method: 'PUT',
         headers: {
-        'Authorization': `Bearer `+ at,
+        'Authorization': "Bearer "+ at,
         "Content-type": "image/jpeg"
         },
-        body: new URLSearchParams({
-            data: dataURL
-        })
+        body: dataURL
     })
-    const res = (await response.json())
-    console.log("stuff!!------",res)
-    console.log("new playlist id:",playlistID)
+    console.log(response)
 }
 
