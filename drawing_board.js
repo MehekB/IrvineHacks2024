bc7fdc93d1930f13f2ccce8c053cd3089f75b7e4
saraht-hub <61632695+saraht-hub@users.noreply.github.com>
2024-02-07 16:32:09 -0800

    changed canvas and ss size

--- drawing_board.js
+++ drawing_board.js
@@ -3,8 +3,8 @@ function captureBoard(){
         // Set the dimensions of the area you want to crop
         const left = 20;   // Adjust these values as needed
         const top = 95;
-        const width = 1140;
-        const height = 1140;
+        const width = 900;
+        const height = 900;
 
         // Create a new canvas to hold the cropped image
         const croppedCanvas = document.createElement('canvas');
