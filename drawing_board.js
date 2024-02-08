4230780eba50a021772adf17cca43339f0900176
saraht-hub <61632695+saraht-hub@users.noreply.github.com>
2024-02-07 16:16:35 -0800

    changed the sizing of the drawing canvas

--- drawing_board.js
+++ drawing_board.js
@@ -3,8 +3,8 @@ function captureBoard(){
         // Set the dimensions of the area you want to crop
         const left = 20;   // Adjust these values as needed
         const top = 95;
-        const width = 900;
-        const height = 900;
+        const width = 910;
+        const height = 910;
 
         // Create a new canvas to hold the cropped image
         const croppedCanvas = document.createElement('canvas');
