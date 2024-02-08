67e5474d6a66e2c994f98891d8352e989fa2c464
saraht-hub <61632695+saraht-hub@users.noreply.github.com>
2024-02-07 16:21:15 -0800

    changed canvas to 1140 width and height

--- drawing_board.js
+++ drawing_board.js
@@ -3,8 +3,8 @@ function captureBoard(){
         // Set the dimensions of the area you want to crop
         const left = 20;   // Adjust these values as needed
         const top = 95;
-        const width = 1040;
-        const height = 1040;
+        const width = 1140;
+        const height = 1140;
 
         // Create a new canvas to hold the cropped image
         const croppedCanvas = document.createElement('canvas');
